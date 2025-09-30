dd60a169155d3906f8946ae47c032e5e9f2cc089
jitesh <iamjiteshsaini2004@gmail.com>
2025-09-30 12:17:52 +0530

    updated

--- Assignments/function questions in c++/6.cpp
+++ Assignments/function questions in c++/6.cpp
@@ -2,20 +2,24 @@
 
 #include <iostream>
 using namespace std;
-void swap(int a, int b) {
+
+void swap(int a, int b)
+{
     int temp = a;
     a = b;
     b = temp;
-    cout<<"after swapping inside function : "<<a<<" "<<b<<endl;
+    cout << "Swapping value : " << a << b << endl;
 }
-int main() {
+
+int main()
+{
     int val1, val2;
-    cout<<"enter value 1 : ";
-    cin>>val1;
-    
-    cout<<"enter value 2 : ";
-    cin>>val2;
-    
+    cout << "enter first value : ";
+    cin >> val1;
+
+    cout << "enter second value : ";
+    cin >> val2;
+
+    cout << "Your entered value : " << val1<< val2 << endl;
     swap(val1, val2);
-    cout<<"after swapping inside main function : "<<val1<<" "<<val2<<endl;
 }
